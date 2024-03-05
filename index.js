d33bd6b5b641bb330f5fbb8fd71e503e9a1f4cd2
Sarah Azad <aras.azad3@gmail.com>
2024-03-05 20:29:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun("go swimming"));
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+mondayWork();
+
+function wrapAdjective(sign = "*") {
+  return function (whatever = "special") {
+    return `You are ${sign}${whatever}${sign}!`;
+  };
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
 // code your solution here
